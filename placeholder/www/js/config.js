d42d56e9773ae03a5f743a12a6a87c9e137c2619
Maxens Dubois <maxens.dubois@gmail.com>
2014-05-15 16:40:38 +0200

    new things quickly

--- placeholder/www/js/config.js
+++ placeholder/www/js/config.js
@@ -45,4 +45,23 @@ jQuery(function() {
 			}
 		});
 
+	//sticky
+	var stickyNavTop = $('#nav').offset().top;  
+  
+	var stickyNav = function(){  
+
+		var scrollTop = $(window).scrollTop();  
+		       
+		if (scrollTop > stickyNavTop) {   
+		    $('#nav').addClass('sticky');  
+		} else {  
+		    $('#nav').removeClass('sticky');   
+		}  
+	};  
+		  
+	stickyNav();  
+		  
+	$(window).scroll(function() {  
+	    stickyNav();  
+	});   
 });
